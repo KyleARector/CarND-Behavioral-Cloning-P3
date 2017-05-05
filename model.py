f2648af8dcd5a7f311fc7b68f9ceedd4472407cf
Kyle Rector <kylearector@outlook.com>
2017-05-04 21:19:02 -0400

    feat: Loading all camera images and implementing Nvidia model

--- model.py
+++ model.py
@@ -16,22 +16,29 @@ with open("Training_Data/driving_log.csv") as in_file:
 # Add images and measurements to lists
 images, measurements = [], []
 for line in lines:
-    data_path = line[0]
-    filename = data_path.split('/')[-1]
-    curr_path = "Training_Data/IMG/" + filename
-    image = cv2.imread(curr_path)
-    images.append(image)
-    measurements.append(float(line[4]))
+    for i in range(3):
+        data_path = line[i]
+        filename = data_path.split('/')[-1]
+        curr_path = "Training_Data/IMG/" + filename
+        image = cv2.imread(curr_path)
+        images.append(image)
+        if i == 1:
+            measurements.append(float(line[4]) + 0.2)
+        elif i == 2:
+            measurements.append(float(line[4]) - 0.2)
+        else:
+            measurements.append(float(line[4]))
 
 
 # Double the training set by flipping each image and measurement
 aug_images, aug_measurements = [], []
-for imate, measurement in zip(images, measurements):
+for image, measurement in zip(images, measurements):
     aug_images.append(image)
     aug_measurements.append(measurement)
     aug_images.append(cv2.flip(image, 1))
     aug_measurements.append(measurement * -1.0)
 
+print(len(aug_images))
 
 X_train = np.array(aug_images)
 y_train = np.array(aug_measurements)
@@ -43,13 +50,17 @@ model.add(Lambda(lambda x: x/255.0 - 0.5,
                  input_shape=(160, 320, 3)))
 # Crop the images to focus on area of interest
 model.add(Cropping2D(cropping=((70, 25), (0, 0))))
-model.add(Convolution2D(6, 5, 5, activation="relu"))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6, 5, 5, activation="relu"))
-model.add(MaxPooling2D())
+#model.add(Convolution2D(6, 5, 5, activation="relu"))
+#model.add(MaxPooling2D())
+model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Convolution2D(64, 3, 3, activation="relu"))
+model.add(Convolution2D(64, 3, 3, activation="relu"))
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss="mse",
@@ -57,7 +68,7 @@ model.compile(loss="mse",
 model.fit(X_train, y_train,
           validation_split=0.2,
           shuffle=True,
-          nb_epoch=3)
+          nb_epoch=5)
 
 # # # End Model # # #
 
